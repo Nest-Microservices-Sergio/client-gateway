c08ab35aed85003af47e51f603e1e7587e2877d3
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-12 11:35:39 -0700

    exception filter personalizado

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -10,8 +10,8 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common';
 import { PRODUCT_SERVICE } from 'src/config';
 
@@ -35,16 +35,12 @@ export class ProductsController {
   }
 
   @Get(':id')
-  async findProduct(@Param('id') id: string) {
-    try {
-      const product = await firstValueFrom(
-        this.productsClient.send({ cmd: 'find_one_product' }, { id }),
-      );
-
-      return product;
-    } catch (error) {
-      throw new BadRequestException(error);
-    }
+  findProduct(@Param('id') id: string) {
+    return this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
   }
 
   @Delete(':id')
