c4e7d6298efa905920f58c346ba339517dbd24b2
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-13 08:17:10 -0700

    expansión de pagination dto

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -6,10 +6,11 @@ import {
   Param,
   Inject,
   ParseUUIDPipe,
+  Query,
 } from '@nestjs/common';
 import { ORDER_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
-import { CreateOrderDto } from './dto';
+import { CreateOrderDto, OrderPaginationDto } from './dto';
 import { catchError } from 'rxjs';
 
 @Controller('orders')
@@ -24,8 +25,8 @@ export class OrdersController {
   }
 
   @Get()
-  findAll() {
-    return this.ordersClient.send('findAllOrders', {});
+  findAll(@Query() orderPaginationDto: OrderPaginationDto) {
+    return this.ordersClient.send('findAllOrders', orderPaginationDto);
   }
 
   @Get(':id')
