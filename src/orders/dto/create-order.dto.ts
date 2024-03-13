3d1e2f56b547235816a65e43ca8b23730d605029
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-12 21:52:03 -0700

    order dto, enum de status de orden

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,1 +1,31 @@
-export class CreateOrderDto {}
+import { Type } from 'class-transformer';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
+import { OrderStatus, OrderStatusList } from '../enum/order.enum';
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  @Type(() => Number)
+  totalAmount: number;
+
+  @IsNumber()
+  @IsPositive()
+  @Type(() => Number)
+  totalItems: number;
+
+  @IsEnum(OrderStatusList, {
+    message: `Possible status values are ${OrderStatusList}`,
+  })
+  @IsOptional()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @IsBoolean()
+  @IsOptional()
+  paid: boolean = false;
+}
