43fecbcae8ae4bbf0bce5ca21049639a7da93b1e
Sergio Barreras <sergiobg.isc@gmail.com>
2024-03-24 16:10:52 -0700

    enum de order actualizada

--- src/orders/enum/order.enum.ts
+++ src/orders/enum/order.enum.ts
@@ -2,10 +2,12 @@ export enum OrderStatus {
   PENDING = 'PENDING',
   DELIVERED = 'DELIVERED',
   CANCELLED = 'CANCELLED',
+  PAID = 'PAID',
 }
 
 export const OrderStatusList = [
   OrderStatus.PENDING,
   OrderStatus.DELIVERED,
   OrderStatus.CANCELLED,
+  OrderStatus.PAID,
 ];
